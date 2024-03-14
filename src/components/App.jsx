257ac7f64b11b1b06050a81a45574cc83d92fa71
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-14 17:15:24 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,51 @@
 import { Routes, Route } from "react-router-dom"
 import Navigation from "./Navigation/Navigation"
-import HomePage from "../pages/HomePage";
-import MoviesPages from "../pages/MoviesPages"
-import NotFoundPage from "../pages/NotFoundPage";
+import Loader from "./Loader/Loader";
+import { Suspense, lazy } from "react"
+
+
+
+const HomePage = lazy(() => import('../pages/HomePage'));
+
+const MoviesPages = lazy(() => import('../pages/MoviesPage'));
+
+const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage'))
+
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
+
+const MovieCast = lazy(() => import('../components/MovieCast/MovieCast'));
+
+const MovieReviews = lazy(() => import('../components/MovieReviews/MovieReviews'))
 
 
 export default function App() {
   return (
-    <div>
+    <>
       <h1>Film search</h1>
 
       <Navigation />
       
+      <div>
+        <Suspense fullback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPages />} />
+            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<MovieCast />} />
+              <Route path="reviews" element={<MovieReviews />} />
 
-      <Routes>
-        <Route path="/" element={ <HomePage/> } />
-        <Route path="/movies" element={ <MoviesPages/>} />
-        <Route path="*" element={ <NotFoundPage />} />
-      </Routes>
-    </div>
+
+            </Route>
+            {/* <Route path="/movies/:movieId" element={<MoviesDetailsPage />}>
+
+              <Route path="cast" element={<MovieInfo />} />
+              <Route path="reviews" element={<MovieReviews />} />
+            </Route> */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+        
+      </div>
+    </>
   );
 }
