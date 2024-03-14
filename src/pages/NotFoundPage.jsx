dfc499a2122734592e3fe90c87fc5f3848de689e
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-14 17:17:34 +0100

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,9 @@
+import { Link } from 'react-router-dom'
+
 export default function NotFoundPage() {
     return (
-        <h1>Page not found!</h1>
-    )
+        <>
+            <Link to="/">Go back</Link>
+        </>
+    );
 }
