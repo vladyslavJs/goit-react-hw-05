58866fc1cbd0aac862531c4db19ac676f972ae25
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-14 17:17:12 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,41 @@
+import fetchMoviesData from "../movies-api";
+import MovieList from "../components/MovieList/MovieList";
+import ErrorMessage from "../components/ErrorMessage/ErrorMessage";
+import Loader from "../components/Loader/Loader";
+
+
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                setIsLoading(true);
+                setError(false);
+                const data = await fetchMoviesData('/trending/movie/day');
+                setMovies(data.results);
+            } catch (error) {
+                setError(true);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        getData();
+    }, []);
+
+
     return (
-        <div>Trending today</div>
+        <>
+            <div>Trending today</div>
+            {loading && <Loader />}
+            {error && <ErrorMessage />}
+            
+            <MovieList movies={movies} />
+        </>   
     );
 }
