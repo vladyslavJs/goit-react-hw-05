e8fbe988876bcca84c8fe523f8498c4357c9e614
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-14 17:16:09 +0100

    Create-movie-card

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,19 @@
+export default function MovieCard({
+    movie: {title, poster_path, vote_average},
+}) {
+    const defaultImg = 
+        'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg';
+    return (
+        <>  
+            <p>{Math.floor(vote_average)} / 10 ⭐⭐⭐</p>
+            <img
+                src={poster_path
+                    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                    : defaultImg
+                }
+                alt={`${title} poster`}
+                width={260}
+            />
+        </>
+    );  
+}
