63828998ef6cb0d139280e3086c0621dbb7b3bcc
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-14 17:17:02 +0100

    Update SearchMovies.jsx

--- src/components/SearchMovies/SearchMovies.jsx
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -1,12 +1,33 @@
+import { useSearchParams } from 'react-router-dom';
+
 export default function SearchMovies() {
+    const [params, setParams] = useSearchParams();
+    const queryFilter = params.get('query') ?? '';
+
+    const changeFilter = newFilter => {
+        params.set('query', newFilter.trim());
+        setParams(params);
+    }
+    
+    const hundleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+
+        form.reset();
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={hundleSubmit}>
                 <label>
-                    <input type="text"
+                    <input
+                        type="text"
                         name="search"
                         autoFocus
-                        placeholder="Enter the movie title" />
+                        placeholder="Enter the movie title"
+                        value={queryFilter}
+                        onChange={e => changeFilter(e.target.value)}
+                    />
                 </label>
             </form>
         </div>
