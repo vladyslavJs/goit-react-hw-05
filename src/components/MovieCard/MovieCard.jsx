0d7e761497f9242cf46839d257ee4de91c9a5c06
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-17 17:43:41 +0100

    update-movieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,11 +1,13 @@
+import css from './MovieCard.module.css'
+
 export default function MovieCard({
     movie: {title, poster_path, vote_average},
 }) {
     const defaultImg = 
         'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg';
     return (
-        <>  
-            <p>{Math.floor(vote_average)} / 10 ⭐⭐⭐</p>
+        <div>  
+            <p className={css.raiting}>{Math.floor(vote_average)} / 10 ⭐⭐⭐</p>
             <img
                 src={poster_path
                     ? `https://image.tmdb.org/t/p/w500${poster_path}`
@@ -13,7 +15,10 @@ export default function MovieCard({
                 }
                 alt={`${title} poster`}
                 width={260}
+                
+                className={css.img}
             />
-        </>
+        </div>
     );  
 }
+
