523ad5367573b8befc38c4251d0857232cd52f1e
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-17 17:44:31 +0100

    update-nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,19 +2,27 @@ import { NavLink } from "react-router-dom"
 import css from './Navigation.module.css'
 import clsx from "clsx";
 
+import { IoHomeOutline } from "react-icons/io5";
+import { PiFilmSlate } from "react-icons/pi";
+import { SiThemoviedatabase } from "react-icons/si";
+
 const activeLink = ({ isActive}) => {
     return clsx(css.link, isActive && css.isActive);
 }
 
 export default function Navigation() {
     return (
-        <nav className={css.nav}>
+        <div className={css.container}>
+             <nav className={css.nav}>
             <NavLink to="/" className={activeLink}>
-                Home
+                Home <IoHomeOutline />
             </NavLink>
             <NavLink to="/movies" className={activeLink}>
-                Movies
+                Movies <PiFilmSlate />
             </NavLink>
+                <span className={css.span}><SiThemoviedatabase className={css.icon } /></span>
         </nav>
+        </div>
+       
     );
 }
