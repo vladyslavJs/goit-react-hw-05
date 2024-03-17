2d9765de6a55a46de90d7f0dbd1f44a90c52c94f
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-17 17:45:11 +0100

    create-home-page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,45 @@
+import fetchMoviesData from "../../movies-api";
+import MovieList from "../../components/MovieList/MovieList";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import Loader from "../../components/Loader/Loader";
+import css from "./HomePage.module.css"
+
+import { ImFire } from "react-icons/im";
+
+
+import { useEffect, useState } from "react";
+
+export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                setIsLoading(true);
+                setError(false);
+                const data = await fetchMoviesData('/trending/movie/day');
+                setMovies(data.results);
+            } catch (error) {
+                setError(true);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        getData();
+    }, []);
+
+
+    return (
+        <div>
+            <h1 className={css.title}>Trending today <ImFire className={css.icon} /></h1>
+            {loading && <Loader />} 
+            {error && <ErrorMessage />}
+            
+            <MovieList movies={movies} />
+            
+        </div>   
+    );
+}
